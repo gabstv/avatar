770178880f2977ed6a986b518874a112c7503d35
Gabriel Ochsenhofer <gabs@gabs.tv>
2017-06-21 11:26:28 -0300

    png function

--- avatar.go
+++ avatar.go
@@ -1,1 +1,16 @@
 package avatar
+
+import (
+	"bytes"
+	"image/png"
+)
+
+func GenerateBasicPNG(seed string) []byte {
+	g := DefaultGen{}
+	img := g.Generate(Config{
+		Seed: seed,
+	})
+	b := new(bytes.Buffer)
+	png.Encode(b, img)
+	return b.Bytes()
+}
